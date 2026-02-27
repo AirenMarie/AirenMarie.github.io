a87bfa6c2c3d9d7618918b0d58512225ba02a68a
AirenMarie <astringer92@gmail.com>
2026-02-27 00:07:31 -0500

    almost there...

--- front/js/index.js
+++ front/js/index.js
@@ -5,16 +5,17 @@
 const viewProjectsBtn = document.getElementById("view-projects");
 async function getRepos() {
   try {
-    const endpoint = "https://localhost:8080/api/v1/fetchData";
+    const endpoint = "http://localhost:8080/api/v1/getRepos";
 
     const response = await fetch(endpoint);
-    const data = await response.json();
+    const result = await response.json();
+    console.log("The server sent back:", result);
 
-    data.forEach((repo) => {
+    result.data.forEach((repo) => {
       const projectSection = document.getElementById("project-section");
       const projectDesc = document.querySelector("project-desc");
 
-      projectSection.innerHTML = `<div class="project"><h3 class="project-name">${repo.name}</h3><p class="project-desc">${repo.description}</p><a href="${repo.html_url}">View Project</a></div>`;
+      projectSection.innerHTML += `<div class="project"><h3 class="project-name">${repo.name}</h3><p class="project-desc">${repo.description}</p><a href="${repo.html_url}">View Project</a></div>`;
 
       if (repo.description === null) {
         projectDesc.textContent = "No description available.";
@@ -25,4 +26,4 @@ async function getRepos() {
   }
 }
 
-viewProjectsBtn.addEventListener("click", fetchData());
+viewProjectsBtn.addEventListener("click", getRepos);
--- server.js
+++ server.js
@@ -14,11 +14,11 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.static("./front"));
+app.use(express.static("./"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-async function fetchData() {
+async function getRepos() {
   const endpoint = `https://api.github.com/users/AirenMarie/repos?per_page=100`;
   try {
     const response = await fetch(endpoint);
@@ -31,11 +31,19 @@ async function fetchData() {
   }
 }
 
-app.use("/api/v1/fetchData", async (request, response) => {
-  response.status(200).json({
-    status: 200,
-    data: await fetchData(),
-  });
+app.get("/api/v1/getRepos", async (request, response) => {
+  try {
+    const repoList = await getRepos();
+
+    console.log("Server found repos:", repoList);
+
+    response.status(200).json({
+      status: 200,
+      data: repoList,
+    });
+  } catch (error) {
+    response.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(port, () => {
